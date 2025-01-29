a684b93b4907333dd81dbfebbbc52de67cbba8a2
Vinicius Martins <chagasdovi@gmail.com>
2025-01-28 22:38:16 -0300

    frist commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const{randomUUID} = require('crypto')
+
+const user = {
+  name: "Gabriel sena",
+  age: 22,
+  email: "gabrielsena@gmail.com",
+}
+
+
+function soma(num1, num2) {
+  return num1 / num2
+}
+
+  function singUp (user){
+    const handledIser = {id: randomUUID(), ...user}
+    console.log(user)
+    console.log(handledIser)
+  }
+
+
+
+
+singUp(user)
