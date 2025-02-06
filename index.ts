80d6dc0a3f96de3205aeacb13443b6c1ec4a05cd
Vinicius Martins <chagasdovi@gmail.com>
2025-02-06 00:05:44 -0300

    criando login "SigIn e SignUp"

--- index.ts
+++ index.ts
@@ -1,48 +1,19 @@
-import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
-import {compare, hash } from'bcrypt'
-import {randomUUID} from"node:crypto"
+import Fastify, { FastifyReply, FastifyRequest } from "fastify"
+import { signIn, signUp } from "./src/routes/auth"
 const app = Fastify()
 
-
-
-interface IUser {
-  id: string
-  name: string
-  username: string
-  password: string
-  email: string
-}
-
-async function signUp(
-  request: FastifyRequest<{ Body: IUser }>,
-  reply: FastifyReply
-
-) {
-  const newUser: IUser = {
-  id: randomUUID(),
-  password: await hash(request.body.password, 10),
-  email: request.body.email,
-  name: request.body.name,
-  username: request.body.username
-  
-  }
-
-  return reply.send(newUser)
-
-}
-
 // Declare a route
-app.post('/user/signUp',signUp)
-
+app.post("/auth/signup", signUp)
+app.post("/auth/signin", signIn)
 
 const start = async () => {
   try {
-    await app.listen({ port: 3333, host: "0.0.0.0" });
-    console.log(" Servidor rodando em http://localhost:3333");
+    await app.listen({ port: 3333, host: "0.0.0.0" })
+    console.log(" Servidor rodando em http://localhost:3333")
   } catch (err) {
-    app.log.error(err);
-    process.exit(1);
+    app.log.error(err)
+    process.exit(1)
   }
-};
+}
 
-start();
+start()
